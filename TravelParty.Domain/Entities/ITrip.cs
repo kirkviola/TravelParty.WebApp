2a8629bd0a53434a5f152fc38f6eca369cdafb88
Matthew Kirkendall <kirk.viola@gmail.com>
2023-09-03 15:23:49 -0500

    Designed entity interfaces in domain, implemented with annotations in infrastructure

---
+++ TravelParty.Domain/Entities/ITrip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelParty.Domain.Entities
+{
+    public interface ITrip
+    {
+        public string Id { get; set; }
+        public string TripName { get; set; }
+        public IEnumerable<IUser>? Travellers { get; set; }
+        public IEnumerable<IUser>? Viewers { get; set; }
+    }
+}
